c576344de46982986868896d38002d21fcceef13
“cmh” <874220809@qq.com>
2021-06-26 17:50:29 +0800

    “首页底部”

--- components/common/commonFoot.vue
+++ components/common/commonFoot.vue
@@ -1,16 +1,81 @@
 <template>
-    <div class="foot-wrap">底部</div>
+  <div class="foot-wrap">
+    <p class="title f30-c242424">开源软件许可证</p>
+    <div class="software-license-list flex-between-center">
+        <div class="software-license-item">
+            <img src="~/assets/images/home/BSD.png" alt="">
+        </div>
+        <div class="software-license-item">
+            <img src="~/assets/images/home/MIT.png" alt="">
+        </div>
+        <div class="software-license-item">
+            <img src="~/assets/images/home/Mozilla.png" alt="">
+        </div>
+        <div class="software-license-item">
+            <img src="~/assets/images/home/Apache.png" alt="">
+        </div>
+        <div class="software-license-item">
+            <img src="~/assets/images/home/LGPL.png" alt="">
+        </div>
+        <div class="software-license-item">
+            <img src="~/assets/images/home/GPL.png" alt="">
+        </div>
+    </div>
+    <div class="bottom flex-between-center">
+        <div class="bottom-left">
+            <p class="f18-c242424">开发文档</p>
+            <p class="f18-c242424">应用市场</p>
+            <p class="f18-c242424">影视资源</p>
+        </div>
+        <div class="bottom-right f14-c8F8F8F">
+            MACCMS Pro  永久免费影视源码
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+    data(){
+        return{
+        }
+    }
+};
 </script>
 
 <style lang="less" scoped>
-.foot-wrap{
-    width: 100%;
-    height: 100px;
+.foot-wrap {
+  width: 100%;
+  height: 350px;
+  padding: 41px 253px 55px 266px;
+  background: #ffffff;
+  .title{
+      padding-left: 30px;
+      margin-bottom: 42px;
+  }
+  .software-license-list{
+      padding: 6px 0;
+      border-top: 1px solid #979797;
+      border-bottom: 1px solid #979797;
+    .software-license-item{
+        width: 217px;
+        // height: 117px;
+        img{
+            width: 100%;
+        }
+    }
+  }
+  .bottom{
+      padding: 20px 27px 0 30px;
+      .bottom-left{
+          display: flex;
+          p{
+              margin-right: 20px;
+              &:last-child{
+                  margin-right:0;
+              }
+          }
+      }
+  }
 }
 </style>
